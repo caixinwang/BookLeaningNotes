dd34e5ffa9b88489e11d2933f4f7c8b9ca4fd00d
caixinwang <caixinwang2002@163.com>
2023-10-25 15:51:37 +0800

    1
    111

--- effectiveJava/EffectiveJavaProgram/src/Chapter01/code01.java
+++ effectiveJava/EffectiveJavaProgram/src/Chapter01/code01.java
@@ -9,6 +9,6 @@ public class code01 {
     // 1.可以通过静态构造器的名称见名知意，构造器没有这个好处
     // 2.不需要每次都创建新的对象，Boolean对象的valueOf方法就使用了这个技术。能够为相同的调用返回相同的对象
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
